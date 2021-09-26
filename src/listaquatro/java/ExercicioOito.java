055ad43cb0d3ddbb9679f8371d60412346558b05
Marina <mari.santanaa@hotmail.com>
2021-09-26 19:08:42 -0300

    Lista quatro corrigida

--- src/listaquatro/java/ExercicioOito.java
+++ src/listaquatro/java/ExercicioOito.java
@@ -1,64 +1,33 @@
 package listaquatro.java;
 
-import java.util.Arrays;
-
 public class ExercicioOito {
-    private String[] nomes = {"Maria", "José", "Joaquim", "Carlos", "Roberta"};
-    private double[] alturas = {1.60, 1.70, 1.66, 1.74, 1.80};
-    private double[] pesos = {50, 74, 68, 76, 80};
-    private double[] imc = new double[5];
-
-    public ExercicioOito() {
-
+    public static void main(String[] args) {
+        ExercicioOito aplicacao = new ExercicioOito();
+        aplicacao.questaoOito();
     }
 
-    public ExercicioOito(String[] nomes, double[] alturas, double[] pesos) {
-        this.nomes = nomes;
-        this.alturas = alturas;
-        this.pesos = pesos;
-    }
-
-    public String[] getNomes() {
-        return nomes;
-    }
-
-    public void setNomes(String[] nomes) {
-        this.nomes = nomes;
-    }
-
-    public double[] getAlturas() {
-        return alturas;
-    }
-
-    public void setAlturas(double[] alturas) {
-        this.alturas = alturas;
-    }
-
-    public double[] getPesos() {
-        return pesos;
-    }
-
-    public void setPesos(double[] pesos) {
-        this.pesos = pesos;
-    }
-
-    @Override
-    public String toString() {
-        return "ExercicioOito{" +
-                "nomes=" + Arrays.toString(nomes) +
-                ", alturas=" + Arrays.toString(alturas) +
-                ", pesos=" + Arrays.toString(pesos) +
-                '}';
-    }
-
-    public void calculo() {
-        for (int i = 0; i < 5; i++) {
-            double imc = pesos[i] / (alturas[i] * alturas[i]);
-            if ((imc < 18.5) || (imc > 25)) {
-                System.out.println("Quem está fora do peso ideal: " + nomes[i]);
+    private void questaoOito() {
+        // Escreva um programa que lê nome, peso e altura de 5 pessoas,
+        // calcula o IMC de cada um e ao final informa se alguém
+        // está fora do peso ideal (o IMC ideal é entre 18.5 e 25).
+        TesteExercicioOito[] pessoas = iniciarArrayPessoas();
+        for (int i = 0; i < pessoas.length; i++) {
+            double imc = pessoas[i].getPeso() / Math.pow(pessoas[i].getAltura(), 2);
+            if (imc < 18.5 || imc > 25) {
+                System.out.printf("%s está fora do peso ideal com imc = %.2f.%n",
+                        pessoas[i].getNome(), imc);
             }
         }
     }
 
+    private TesteExercicioOito[] iniciarArrayPessoas() {
+        String[] nomes = {"Joao", "Jose", "Jota", "Junior", "Janio"};
+        double[] alturas = {1.60, 1.70, 1.90, 1.82, 1.68};
+        double[] pesos = {92, 70, 60, 75, 48};
+       TesteExercicioOito[] pessoas = new TesteExercicioOito[][5];
+        for (int i = 0; i < pessoas.length; i++) {
+            pessoas[i] = new TesteExercicioOito(nomes[i], pesos[i], alturas[i]);
+        }
+        return pessoas;
+    }
 }
-
--- src/listaquatro/java/TesteExercicioOito.java
+++ src/listaquatro/java/TesteExercicioOito.java
@@ -1,11 +1,50 @@
 package listaquatro.java;
 
 public class TesteExercicioOito {
-    public static void main(String[] args) {
-        ExercicioOito calculoImc = new ExercicioOito();
-        System.out.println(calculoImc);
+    private String nome;
+    private double peso;
+    private double altura;
 
-        calculoImc.calculo();
+    public TesteExercicioOito() {
+    }
+
+    public TesteExercicioOito(String nome, double peso, double altura) {
+        this.nome = nome;
+        this.peso = peso;
+        this.altura = altura;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "nome='" + nome + '\'' +
+                ", peso=" + peso +
+                ", altura=" + altura +
+                '}';
     }
 }
 
